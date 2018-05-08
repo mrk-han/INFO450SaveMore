577e351c038663ff71dfd0f955129da7bd492027
unknown <markwhan11@gmail.com>
2018-05-08 02:46:28 -0400

    add logic to Savings account

--- INFO450SaveMore/Savings.cpp
+++ INFO450SaveMore/Savings.cpp
@@ -1,1 +1,56 @@
 #include "stdafx.h"
+#include "Savings.h"
+
+Savings::Savings(int number, double balance) : Account(number, balance)
+{
+	// must calculate interest when created
+	calculateInterest();
+}
+
+int Savings::depositFunds(double depositAmount)
+{
+	// Have to deposit ammount above 0, if it is above 0 then add to accountBalance.
+	if (depositAmount > 0)
+	{
+		accountBalance += depositAmount;
+		//CALCULATE INTEREST HERE AFTER DEPOSIT TO SEE IF RAISED A LEVEL
+		calculateInterest();
+
+		return 0;
+	}
+	// Cannot deposit negative numbers!
+	else if (depositAmount <= 0)
+	{
+		return -1;
+	}
+}
+
+int Savings::withdrawFunds(double withdrawAmount)
+{
+	//cannot withdraw funds from negative balance
+	if ((accountBalance - withdrawAmount - WITHDRAWAL_FEE) < 0)
+	{
+		return -1;
+	}
+	else if ((accountBalance - (withdrawAmount + WITHDRAWAL_FEE)) >= 0)
+	{
+		accountBalance -= (withdrawAmount + WITHDRAWAL_FEE);
+		calculateInterest();
+		return 0;
+	}
+}
+
+void Savings::calculateInterest()
+{
+	if (accountBalance <= 10000)
+	{
+		interestRate = ONE_PERCENT_APR;
+	}
+	else if (accountBalance >= 10000)
+	{
+		interestRate = TWO_PERCENT_APR;
+	}
+	//https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
+	accountBalance += (accountBalance*(interestRate / 12));
+}
+
--- INFO450SaveMore/Savings.h
+++ INFO450SaveMore/Savings.h
@@ -1,1 +1,19 @@
 #pragma once
+# include "Account.h"
+
+class Savings : public Account
+{
+private:
+	const double WITHDRAWAL_FEE = 2.00;
+	const double ONE_PERCENT_APR = 0.01;
+	const double TWO_PERCENT_APR = 0.02;
+
+
+public:
+	Savings(int number, double balance);
+	int depositFunds(double depositAmount);
+	int withdrawFunds(double withdrawAmount);
+	void calculateInterest();
+
+
+};

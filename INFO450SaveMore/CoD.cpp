fba49520be5b32228362ef029f55120441ab7104
unknown <markwhan11@gmail.com>
2018-05-08 02:56:13 -0400

    add logic for CoD account

--- INFO450SaveMore/CoD.cpp
+++ INFO450SaveMore/CoD.cpp
@@ -1,1 +1,22 @@
 #include "stdafx.h"
+#include "CoD.h"
+
+CoD::CoD(int number, double balance, int term) : Account(number, balance)
+{
+	if (term < 5)
+	{
+		// Less than 5 year rate for anything other than 5 year term (per requirements)
+		interestRate = LESS_THAN_FIVE_YEAR_RATE;
+	}
+	else if (term >= 5)
+	{
+		// if term is 5 or greater, then give 5 year rate (per requirements)
+		interestRate = FIVE_YEAR_RATE;
+	}
+	calculateInterest();
+}
+
+void CoD::calculateInterest()
+{
+	accountBalance += ((accountBalance)*(interestRate / 12));
+}
--- INFO450SaveMore/CoD.h
+++ INFO450SaveMore/CoD.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Account.h"
+
+class CoD : public Account
+{
+private:
+	const double FIVE_YEAR_RATE = 0.10;
+	const double LESS_THAN_FIVE_YEAR_RATE = 0.05;
+	int term; // CANNOT BE CHANGED AFTER CREATION (per requirements)
+
+public:
+	CoD(int number, double balance, int term);
+	void calculateInterest();
+};
